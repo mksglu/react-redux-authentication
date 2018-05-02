9829cded77dfe0e0336159597bc295d552140372
Mert Koseoglu <info@mkoseoglu.com>
2018-05-02 04:41:17 +0300

    token

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -2,9 +2,8 @@ import decode from 'jwt-decode'
 
 import { LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from '../constants/loginConstants'
 // const user = JSON.parse(localStorage.getItem('user'))
-const user = localStorage.getItem('user')
-
-const initialState = user ? { loggedIn: true, user: decode(user.username) } : {}
+const user = localStorage.getItem('user') ? decode(localStorage.getItem('user')) : false
+const initialState = user ? { loggedIn: true, user: user.username } : {}
 
 export default function authentication(state = initialState, action) {
   switch (action.type) {
