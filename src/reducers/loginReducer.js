912e782b8aaef24f6e38b1fbb837aba0c29b1552
Mert Koseoglu <info@mkoseoglu.com>
2018-05-02 04:28:07 +0300

    redux added

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,13 +1,15 @@
 import decode from 'jwt-decode'
 
 import { LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from '../constants/loginConstants'
+// const user = JSON.parse(localStorage.getItem('user'))
+const user = localStorage.getItem('user')
 
-const user = decode(localStorage.getItem('user'))
-console.log(user.username)
+const initialState = user ? { loggedIn: true, user: decode(user.username) } : {}
 
-export default function authentication(state = {}, action) {
+export default function authentication(state = initialState, action) {
   switch (action.type) {
     case LOGIN_SUCCESS:
+      console.log('initialState', action.user)
       return {
         loggedIn: true,
         user: action.user,
